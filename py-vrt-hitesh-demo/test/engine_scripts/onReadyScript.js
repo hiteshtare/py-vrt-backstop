431838ccc9a8f28a226596b365216363c5b3fb17
Hitesh Tare <hiteshtare@gmail.com>
2024-04-18 11:59:29 +0530

    Change to suppport Dynamic testScenarios.length to EnableLogging om console

--- py-vrt-hitesh-demo/test/engine_scripts/onReadyScript.js
+++ py-vrt-hitesh-demo/test/engine_scripts/onReadyScript.js
@@ -1,20 +1,32 @@
 // onReady example (puppeteer engine)
 const basicConfig = require("./../../test/basicConfig");
 // tests/visual-tests/test/basicConfig.js
+
+// Get user defined scenario json
+const scenarioJSON = require("./../../data/scenarios.json");
+
+let testScenarios = [];
+
+scenarioJSON.map((s) => {
+  const obj = {
+    readyEvent: "",
+    readySelector: "",
+    delay:  basicConfig.delayInMS,
+  };
+  testScenarios.push({ ...obj, ...s }); // merging url,obj
+});
+
+
 module.exports = async (page, scenario, vp) => {
   console.enableLogging();
   console.log(`Scenario ${scenario.sIndex + 1} > ${scenario.label} (${vp.label})`);
   // console.warn(`scenario.sIndex: ${scenario.sIndex} | vp.vIndex: ${vp.vIndex}`);
-
-  // if (process.argv.length > 0) { 
-  //   process.argv.forEach(function (val, index, array) {
-  //     console.log(index + ': ' + val);
-  //   });
-  // }
   console.disableLogging();
   
+  // console.warn(`Scenarios length: ${testScenarios.length}`);
+
   //Should be used if there is lazy loading on site, it may slow down the tests
- if ((scenario.sIndex == 43)) {
+ if ((scenario.sIndex == testScenarios.length-1)) {
     console.enableLogging();
   }
 }
