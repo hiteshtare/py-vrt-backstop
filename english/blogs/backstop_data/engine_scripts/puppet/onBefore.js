781e610ab4c3f7860e9fbe80605713538086e13b
Hitesh Tare <hiteshtare@gmail.com>
2024-10-04 12:09:19 +0530

    **Disabled console Log by ignoredMessages - string match

--- english/blogs/backstop_data/engine_scripts/puppet/onBefore.js
+++ english/blogs/backstop_data/engine_scripts/puppet/onBefore.js
@@ -8,6 +8,16 @@
 })(console.log);
 
 module.exports = async (page, scenario, vp) => {
-  await require('./loadCookies')(page, scenario);
+  const ignoredMessages = [
+    "JSHandle"
+  ];
+
+  console.log = (message) => {
+    ignoredMessages.some((ignore) => message.includes(ignore))
+      ? undefined
+      : process.stdout.write(`${message}\n`);
+  };
+
+  await require("./loadCookies")(page, scenario);
   console.disableLogging();
 };
--- english/priority-pages/backstop_data/engine_scripts/puppet/onBefore.js
+++ english/priority-pages/backstop_data/engine_scripts/puppet/onBefore.js
@@ -1,3 +1,13 @@
 module.exports = async (page, scenario, vp) => {
-  await require('./loadCookies')(page, scenario);
+  const ignoredMessages = [
+    "JSHandle"
+  ];
+
+  console.log = (message) => {
+    ignoredMessages.some((ignore) => message.includes(ignore))
+      ? undefined
+      : process.stdout.write(`${message}\n`);
+  };
+
+  await require("./loadCookies")(page, scenario);
 };
