355358895165be73a5ceb2986f44867a6d5d882b
Hitesh Tare <hiteshtare@gmail.com>
2024-10-04 11:38:18 +0530

    #Removed bangla-and-kannada folder it is NOT Required

--- bangla-and-kannada/run-tests.sh
+++
@@ -1,5 +0,0 @@
-# gp site test.yssofindia.org -http-auth-off
-npm run reference-and-tests
-
-echo "VRT for BANGLA-AND-KANNADA: https://vrt.yssofindia.org/vrt/bangla-and-kannada/backstop_data/html_report/index.html"
-# gp site test.yssofindia.org -http-auth
