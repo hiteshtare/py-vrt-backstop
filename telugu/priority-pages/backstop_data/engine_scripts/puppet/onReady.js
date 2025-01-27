169b79537231e271f4256090fabc9ddc3ea0a1f2
Hitesh Tare <hiteshtare@gmail.com>
2025-01-27 17:15:21 +0530

    Replaced scrollPage with onReady

--- telugu/priority-pages/backstop_data/engine_scripts/puppet/onReady.js
+++ telugu/priority-pages/backstop_data/engine_scripts/puppet/onReady.js
@@ -1,5 +1,5 @@
 module.exports = async (page, scenario, vp) => {
-  console.log('SCENARIO > ' + scenario.label);
+  console.log("SCENARIO > " + scenario.label + " (" + vp.label + ")");
   await require('./clickAndHoverHelper')(page, scenario);
 
   // add more ready handlers here...
