9c80139b8ea03fa815094f6610f38c1976e65fd5
Hitesh Tare <hiteshtare@gmail.com>
2025-10-31 16:45:53 +0530

    Added new Type - Important for English only

---
+++ english/important/backstop_data/engine_scripts/puppet/onReady.js
@@ -0,0 +1,6 @@
+module.exports = async (page, scenario, vp) => {
+  console.log("SCENARIO > " + scenario.label + " (" + vp.label + ")");
+  await require('./clickAndHoverHelper')(page, scenario);
+
+  // add more ready handlers here...
+};
